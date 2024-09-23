c8cfaa076672bfde001e34227068fa6a01f0c10f
Anurag Kandulna <dev.ak@outlook.in>
2024-09-23 15:22:43 +0530

    added basics

---
+++ 01_basics.py
@@ -0,0 +1,23 @@
+# Numbers
+a = 10
+b = 10.1
+c = -30.99
+d = 20e4
+e = 34j
+print('Type of a: ', type(a))
+print('Type of b: ', type(b))
+print('Type of c: ', type(c))
+print('Type of d: ', type(d))
+print(d)
+print('Type of e: ', type(e))
+print(e)
+print(e*e)
+
+f = 30 + 4j
+print(f*f*f*f)
+
+# Casting
+x = int(10.5)
+print(x)
+y = float(x)
+print(y)
